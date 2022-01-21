8bfba6027b6a6135fbd24b473195f5f3fb8c12ec
msquillari <miguel.squillari@gmail.com>
2022-01-21 08:48:29 -0300

    agrego modelo de entramiento

--- util/TakePictures.py
+++ util/TakePictures.py
@@ -1,12 +1,23 @@
 import cv2 as cv
 import os
+import sys
 import time
 
 cantPicture: int = 300
 picturedir: str = "c:/Temp/detector/"
 capture = cv.VideoCapture(0)
 pic = 0
-print("path::" , os.path)
+print("path::", os.path.exists(picturedir))
+
+
+if sys.platform.startswith('win'):
+    picturedir = "c:/Temp/detector2/"
+elif sys.platform.startswith('linux'):
+    picturedir = "/home/Temp/detector"
+
+
+if not os.path.exists(picturedir):
+    os.mkdir(picturedir)
 
 while pic < 10:
     ret, img = capture.read()
