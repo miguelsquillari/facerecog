03022624dccab4eb0a347cf0d7c83db0186872e1
msquillari <miguel.squillari@gmail.com>
2022-01-22 22:51:15 -0300

    modif la funcion para que solo detecte y guarde el rostro de la persona.

--- util/TakePictures.py
+++ util/TakePictures.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import os
 import sys
 import time
+import imutils
 
 cantPicture: int = 300
 picturedir: str = "c:/Temp/detector/"
@@ -15,24 +16,50 @@ if sys.platform.startswith('win'):
 elif sys.platform.startswith('linux'):
     picturedir = "/home/Temp/detector"
 
-
+# si no existe la carpeta la creo.
 if not os.path.exists(picturedir):
     os.mkdir(picturedir)
 
+# Flags for video I/O : CAP_DSHOW
+# Python: cv.CAP_DSHOW
+# DirectShow (via videoInput)
+capture = cv.VideoCapture(0, cv.CAP_DSHOW)
+
+
+faceClassifier = cv.CascadeClassifier(cv.data.haarcascades+'haarcascade_frontalface_default.xml')
+count = 0
+
+
+# tomo las pictures
 while pic < 10:
     ret, img = capture.read()
-    print(ret)
-    if ret==False:
+    # print(ret)
+    if ret == False:
         break
 
-    path = picturedir + "rostro_{}".format(pic) + ".jpg"
-    print("ruta: " + path)
+    img = imutils.resize(img, width=640)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    auxFrame = img.copy()
 
-    cv.imwrite(path, img)
-    cv.imshow("Registrando ...", img)
-    pic = pic + 1
-    cv.waitKey(1)
-    time.sleep(0.4)
+    faces = faceClassifier.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        face = auxFrame[y:y + h, x:x + w]
+        face = cv.resize(face, (250, 250), interpolation=cv.INTER_CUBIC)
+        path = picturedir + "rostro_{}".format(pic) + ".jpg"
+        cv.imwrite(path, face)
+        #cv.imwrite(personPath + '/rotro_{}.jpg'.format(count), face)
+        pic = pic + 1
+        cv.waitKey(1)
+        time.sleep(0.4)
+    cv.imshow('imagen', img)
+
+
+    #print("ruta: " + path)
+
+    #cv.imwrite(path, img)
+    #cv.imshow("Registrando ...", img)
+
 
 capture.release()
 cv.destroyAllWindows()
