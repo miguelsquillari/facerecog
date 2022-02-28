22fdc4a9714619f3be9f6d274cf2c90d03a7553e
msquillari <miguel.squillari@gmail.com>
2022-02-27 23:39:30 -0300

    reconocimiento facil

---
+++ Recog.py
@@ -0,0 +1,71 @@
+import cv2
+import os
+
+
+#pathModelo = "C:\Users\migue\PycharmProjects\FaceDetect\trainner\modeloLBPHFace_face.xml"
+dataPath = "C:/Temp/detector2/miguel/"
+imagePaths = os.listdir(dataPath)
+print('imagePaths=', imagePaths)
+
+# face_recognizer = cv2.face.EigenFaceRecognizer_create()
+# face_recognizer = cv2.face.FisherFaceRecognizer_create()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Leyendo el modelo
+# face_recognizer.read('modeloEigenFace.xml')
+# face_recognizer.read('modeloFisherFace.xml')
+val = face_recognizer.read('CmodeloLBPHFace_face.xml')
+print("what ?? ", val)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+# cap = cv2.VideoCapture('Video.mp4')
+val2 = cv2.data.haarcascades
+print("val2", val2)
+cv2.load('haarcascade_frontalface_default.xml')
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'modeloLBPHFace_face.xml')
+
+while True:
+    ret, frame = cap.read()
+    if ret == False: break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    auxFrame = gray.copy()
+
+    faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        rostro = auxFrame[y:y + h, x:x + w]
+        rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
+        result = face_recognizer.predict(rostro)
+
+        cv2.putText(frame, '{}'.format(result), (x, y - 5), 1, 1.3, (255, 255, 0), 1, cv2.LINE_AA)
+        '''
+        # EigenFaces
+        if result[1] < 5700:
+            cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+        else:
+            cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+
+        # FisherFace
+        if result[1] < 500:
+            cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+        else:
+            cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+        '''
+        # LBPHFace
+        if result[1] < 70:
+            cv2.putText(frame, '{}'.format(imagePaths[result[0]]), (x, y - 25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        else:
+            cv2.putText(frame, 'Desconocido', (x, y - 20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    cv2.imshow('frame', frame)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
